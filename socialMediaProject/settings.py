82fd57c32523ec68c4d54a0844ead2f7fa63a63b
appydev6 <ashwinpahade97@gmail.com>
2025-12-29 22:17:06 +0530

    changed the time zone to Asia/Kolkata

--- socialMediaProject/settings.py
+++ socialMediaProject/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'socialMediaProject.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_project",
+        "USER": "postgres",
+        "PASSWORD": "123",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -104,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
