782b3d914d065bd6528dd6cfc0d10645fb3cf125
appydev6 <ashwinpahade97@gmail.com>
2025-12-30 12:19:19 +0530

    auth app renamed to user_auth, installed pillow using uv and updated models

--- media_app/models.py
+++ media_app/models.py
@@ -1,4 +1,31 @@
 from django.db import models
-from auth.models import User
+from user_auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='Profile', on_delete=models.CASCADE)
+    image = models.ImageField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'profile'
+
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name='post', on_delete=models.CASCADE)
+    caption = models.TextField()
+    image = models.ImageField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'post'        
+
+class LikePost(models.Model):
+    user = models.ForeignKey(User, related_name='like_post', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='like_post', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'like_post'
--- socialMediaProject/settings.py
+++ socialMediaProject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth',
+    'user_auth',
     'media_app',
 ]
 
