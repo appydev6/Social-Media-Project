088883628700de5181d9bda8070a3430d7a4143a
appydev6 <ashwinpahade97@gmail.com>
2026-01-14 12:31:11 +0530

    image view is added

--- media_app/urls.py
+++ media_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . views import index_view
+from . views import index_view, submit_post
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('submit-post/', submit_post, name='submit_post'),
 ]
