4d8f05154509056b5ec5b03ec78e564ecbffc741
appydev6 <ashwinpahade97@gmail.com>
2026-01-17 15:02:47 +0530

    like button added and removed like button for the self post.

--- media_app/views.py
+++ media_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from media_app.models import Post
+from django.shortcuts import render, redirect, get_object_or_404
+from media_app.models import Post, LikePost
 from django.contrib.auth.decorators import login_required
 
 def get_data(request, key):
@@ -23,4 +23,10 @@ def submit_post(request):
         caption=content,
         image=image,
     )
+    return redirect('index')
+
+@login_required(login_url='sign_in')
+def like_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    LikePost.objects.get_or_create(user=request.user, post=post)
     return redirect('index')
